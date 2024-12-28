3b9c0dcb2b302c080c9122afd220e8d8426e769d
Angelika Szczerbowska <angelika.szczerbowska@studenci.collegiumwitelona.pl>
2024-12-28 18:50:52 +0100

    Add UUID to Reader and Book

--- src/src/Book.java
+++ src/src/Book.java
@@ -1,16 +1,23 @@
+import java.util.UUID;
+
 public class Book {
+    private UUID id;
     private String title;
     private String author;
     private int year;
     private int availableCopies;
 
-    public Book(String title, String author, int year, int availableCopies) {
+    public Book(UUID id, String title, String author, int year, int availableCopies) {
+        this.id = id;
         this.title = title;
         this.author = author;
         this.year = year;
         this.availableCopies = availableCopies;
     }
 
+    public UUID getId() {
+        return id;
+    }
     public String getTitle() {
         return title;
     }
